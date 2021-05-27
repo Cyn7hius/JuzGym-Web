1981f00730d568589964108c200ec24a8cfaa9c2
Cyn7hius <64959508+Cyn7hius@users.noreply.github.com>
2021-05-27 15:16:25 +0800

    Used template as starting point

---
+++ src/config/firebase.js
@@ -0,0 +1,11 @@
+// Firebase Config
+export const firebaseConfig = {
+    apiKey: "AIzaSyDajcWw6_4ZZfhg4thM7AM3t69cPEcsT_k",
+    authDomain: "juzgym-97e39.firebaseapp.com",
+    databaseURL: "https://juzgym-97e39-default-rtdb.europe-west1.firebasedatabase.app/",
+    projectId: "juzgym-97e39",
+    storageBucket: "juzgym-97e39.appspot.com",
+    messagingSenderId: "474696019670",
+    appId: "1:474696019670:web:a1127e2891be8f0236a377",
+    measurementId: "G-T9ZNGPP98K"
+  };
