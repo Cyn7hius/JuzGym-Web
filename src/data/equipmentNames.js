c54a763cbe9b88cb2c4a0ff778888976f5eb181b
Cyn7hius <64959508+Cyn7hius@users.noreply.github.com>
2021-06-02 19:19:58 +0800

    New buttons

---
+++ src/data/equipmentNames.js
@@ -0,0 +1,29 @@
+export const images = [
+    {
+      id: 1,
+      title: "Dumbbell",
+      excerpt: "Insert Picture of Dumbbell here",
+      url: "https://i.imgur.com/e0msZB9.jpeg",
+    },
+  
+    {
+      id: 2,
+      title: "Resistance Bands",
+      excerpt: "Insert Picture of Bands here",
+      url: "https://i.imgur.com/sCDv3zt.jpg",
+    },
+  
+    {
+      id: 3,
+      title: "Body Weight",
+      excerpt: "Insert Picture of Calvin here",
+      url: "https://images.pexels.com/photos/2294354/pexels-photo-2294354.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    },
+  
+    {
+      id: 4,
+      title: "All",
+      excerpt: "Insert Picture of ??? here",
+      url: "https://images.pexels.com/photos/1431282/pexels-photo-1431282.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    },
+  ];
