cde52cde3b4703a76382160bee2cede4bdb5479c
El0hime <dillontkw@gmail.com>
2021-06-07 18:05:28 +0800

    Added virtuosoList in exerciseList.js
    Added faker dependcy(to test out the example code given, we dont need to add that dependency to master)
    
    Added a temp file for our workouts in data(exerciseDatabase.js)

--- src/components/Fitness/ExerciseList.js
+++ src/components/Fitness/ExerciseList.js
@@ -1,5 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
+import { Virtuoso } from "react-virtuoso";
+import { data } from "../../data/exerciseDatabase";
 
-export default function ExerciseList({id, id2}) {
-  return <h1> {id} EXERCISES: {id2} </h1>;
+function App() {
+  const [isScrolling, setIsScrolling] = useState(false);
+  return (
+    <Virtuoso
+      style={{ height: "400px" }}
+      //Uses the data from json file
+      data={data}
+      isScrolling={setIsScrolling}
+      //Total number of exercises to render
+      totalCount={200}
+      itemContent={(index, user) => (
+        <div
+          style={{
+            backgroundColor: user.bgColor,
+            padding: "1rem 0",
+          }}
+        >
+          {/* This div is for the image */}
+          <div style={{ float: "left", margin: "1rem" }}>
+            {/* {isScrolling ? avatarPlaceholder() : avatar() } */}
+            {isScrolling ? <h4>scrolling</h4> : <h4>Picture of exercise</h4>}
+          </div>
+
+          {/* This div is the title + instructions */}
+          <h4>{user.name}</h4>
+          <div style={{ marginTop: "1rem" }}>{user.instructions}</div>
+        </div>
+      )}
+    />
+  );
+}
+
+export default function ExerciseList({ id, id2 }) {
+  // return <h1> {id} EXERCISES: {id2} </h1>;
+  return <App />;
 }
---
+++ src/data/exerciseDatabase.js
@@ -0,0 +1,38 @@
+export const data = [
+    {
+        "id1": 1,
+        "id2": 5,
+        "name": "Test id1",
+        "instructions": "None",
+        "safety": "None",
+        "variations": "1) Variation 1\n2) Variation 2\n3) Variation 3",
+        "image": "none"
+    },
+    {
+        "id1": 3,
+        "id2": 7,
+        "name": "Test id2",
+        "instructions": "None",
+        "safety": "None",
+        "variations": "1) Variation 1\n2) Variation 2\n3) Variation 3",
+        "image": "none"
+    },
+    {
+        "id1": 1,
+        "id2": 7,
+        "name": "Dumbbell Curl",
+        "instructions": "1. Assume the starting position for the standing dumbbell curl by grasping a pair of dumbbells and standing straight up, feet together and dumbbells by your side. The dumbbells should not be touching your body.\n2. Your palms should facing upwards.\n3. Take up the slack by bending the elbows slightly. Tension should be on the biceps.\n4. Slowly curl the dumbbells up as far as possible.\n5. Squeeze the biceps hard, and then slowly lower the dumbbells back down to the starting position.\n6. Repeat for desired reps.\n\n",
+        "safety": "1. The tension should be on the biceps at all times. Do not let them \"hang\" at the bottom of the exercise.\n2. Keep the rep timing slow and control the weight throughout the set.\n3. Do not let the dumbbells touch your body.\n4. And finally, do not swing the body back as you curl up the weight. The body should stay fixed throughout the movement. Focus on only moving your forearms.",
+        "variations": "1. Hammer Curl\n2. Concentration Curl\n3. Incline Dumbbell Curl",
+        "image": "none"
+    },
+    {
+        "id1": 2,
+        "id2": 6,
+        "name": "Banded Glute Bridge",
+        "instructions": "1. Wrap a band just above your knees and start in a supine position on the floor with your arms at a 45 degree angle relative to your torso.\n2. Bridge your hips up by squeezing your glutes and driving your heels into the floor.\n3. Push out slightly on the band as you bridge up.\n4. Lower your hips back to the starting position and repeat for the desired number of repetitions.",
+        "safety": "1. Ensure that the lower back doesn’t arch as you extend the hip, you should focus on squeezing the glutes and slightly posteriorly tilting the pelvis.\n2. Ideally you should prioritize a straight line from your knees through your shoulders at the peak of contraction.\n3. Focus on exhaling as you bridge the hips up. You shouldn’t feel the movement through your lower back at all.\n4. As you push out on the band, don’t allow the knees to track out past the outside of your feet.\n5. If you can’t seem to feel your glutes activating, palpate the musculature with your hands and focus on pausing the movement at the peak of contraction.\n\n",
+        "variations": "1. Standing External Rotation \n2. Lateral Band Walk \n3. Banded Multi Directional Toe Touch ",
+        "image": "none"
+    }
+]
